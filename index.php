2f986813621ddf0904fd3f434befc5a18d752eb0
pptvvx <605729908@qq.com>
2019-03-11 16:07:45 +0800

    123

--- index.php
+++ index.php
@@ -47,24 +47,33 @@
 //echo date('y/m/d-h:i:sa',$now).'&nbsp;&nbsp;&nbsp;'.'this is now'.'<br>';
 //$myDate=strtotime('next sunday');
 //echo date('y/m/d-h:i:sa',$myDate).'&nbsp;&nbsp;'.'str时间'.'<br>';
-header('Content-type:image/jpeg');
-$width=120;
-$height=40;
-$element=array('a','b','c','d','e','f','g');
-$code=$element[rand(0,count($element)-1)].$element[rand(0,count($element)-1)].$element[rand(0,count($element)-1)];
-$img=imagecreatetruecolor($width,$height);
-$colorBg=imagecolorallocate($img,rand(200,255),rand(200,255),rand(200,255));
-$colorBorder=imagecolorallocate($img,rand(200,255),rand(200,255),rand(200,255));
-$colorString=imagecolorallocate($img,rand(10,100),rand(10,100),rand(10,100));
-imagefill($img,0,0,$colorBg);
-imagerectangle($img,0,0,$width-1,$height-1,$colorBorder);
-for($i=0;$i<100;$i++){
-    imagesetpixel($img,rand(0,$width-1),rand(0,$height-1),imagecolorallocate($img,rand(10,100),rand(10,100),rand(10,100)));
+
+//header('Content-type:image/jpeg');
+//$width=120;
+//$height=40;
+//$element=array('a','b','c','d','e','f','g');
+//$code=$element[rand(0,count($element)-1)].$element[rand(0,count($element)-1)].$element[rand(0,count($element)-1)];
+//$img=imagecreatetruecolor($width,$height);
+//$colorBg=imagecolorallocate($img,rand(200,255),rand(200,255),rand(200,255));
+//$colorBorder=imagecolorallocate($img,rand(200,255),rand(200,255),rand(200,255));
+//$colorString=imagecolorallocate($img,rand(10,100),rand(10,100),rand(10,100));
+//imagefill($img,0,0,$colorBg);
+//imagerectangle($img,0,0,$width-1,$height-1,$colorBorder);
+//for($i=0;$i<100;$i++){
+//    imagesetpixel($img,rand(0,$width-1),rand(0,$height-1),imagecolorallocate($img,rand(10,100),rand(10,100),rand(10,100)));
+//}
+//for($i=0;$i<3;$i++){
+//    imageline($img,rand(0,$width/2),rand(0,$height),rand($width/2,$width),rand(0,$height),imagecolorallocate($img,rand(10,100),rand(10,100),rand(10,100)));
+//}
+//imagettftext($img,14,rand(-30,30),rand(30,50),25,$colorString,'font/Helvetica Bold Oblique.ttf',$code);
+//imagejpeg($img);
+//imagedestroy($img);
+echo 123;
+$servername = "localhost";
+$username = "root";
+$password = "";
+$conn = mysqli_connect($servername, $username, $password);
+if (!$conn) {
+    die("连接失败: " . mysqli_connect_error());
 }
-for($i=0;$i<3;$i++){
-    imageline($img,rand(0,$width/2),rand(0,$height),rand($width/2,$width),rand(0,$height),imagecolorallocate($img,rand(10,100),rand(10,100),rand(10,100)));
-}
-imagettftext($img,14,rand(-30,30),rand(30,50),25,$colorString,'font/Helvetica Bold Oblique.ttf',$code);
-imagejpeg($img);
-imagedestroy($img);
 ?>
